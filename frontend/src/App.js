060bdfee5c6e49d1b6c44ce2883493ecb12e8f81
Atveek <atveekdungarani2021@gmail.com>
2024-03-27 21:39:42 +0530

    Create Routing Login and Signup page Home page UI are created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <RouterProvider router={router} />
     </div>
   );
 }
