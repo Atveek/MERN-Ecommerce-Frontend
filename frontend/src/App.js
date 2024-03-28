31cfd198e6b4541807433832eed7b0456a1bfae1
Atveek <atveekdungarani2021@gmail.com>
2024-03-28 14:41:42 +0530

    checkout and product detail page ui are ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import CartPage from "./pages/CartPage";
-
+import Checkout from "./pages/Checkout";
+import ProductDetailPage from "./pages/ProductDetailPage";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -22,6 +23,14 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage></CartPage>,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout></Checkout>,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
 
